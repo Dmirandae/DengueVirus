e934594e65251f8506b24f5df41c56886ebc2013
AndreaSilva <andreasilvacala@gmail.com>
2017-09-22 15:31:42 +0200

    Se agregaron nuevas lineas para realizar la busqueda de secuencias con 95, 97, 99, 99,5 99,9% de similaridad para el gen E

--- Piloto_Dengue/bin/usearch_similaridad_genE.sh
+++ Piloto_Dengue/bin/usearch_similaridad_genE.sh
@@ -21,6 +21,12 @@ cd LSB/Piloto_Dengue/data/Secuencias_descargadas/Secuencias_genE
 
 ./usearch --cluster GenE_sort.fasta --maxlen 1600 --id 0.97 --iddef 0 --band 1 --seedsout GenE_97_similaridad.fasta --uc result.uc --log log_97.log 
 
+./usearch --cluster GenE_sort.fasta --maxlen 1600 --id 0.95 --iddef 0 --band 1 --seedsout GenE_95_similaridad.fasta --uc result.uc --log log_95.log 
+
+./usearch --cluster GenE_sort.fasta --maxlen 1600 --id 0.99 --iddef 0 --band 1 --seedsout GenE_99_similaridad.fasta --uc result.uc --log log_99.log 
+
+./usearch --cluster GenE_sort.fasta --maxlen 1600 --id 0.995 --iddef 0 --band 1 --seedsout GenE_995_similaridad.fasta --uc result.uc --log log_995.log 
+
 # Conocer las secuencias que fueron eliminadas porque son 99.5% identicas
 
 #Extraer los ids de cada secuencias:
@@ -32,22 +38,52 @@ grep ">" GenE_sort.fasta > GenE_sort_id.fasta
 
 grep ">" GenE_97_similaridad.fasta > GenE_97_similaridad_id.csv
 
+grep ">" GenE_95_similaridad.fasta > GenE_95_similaridad_id.csv
+
+grep ">" GenE_99_similaridad.fasta > GenE_99_similaridad_id.csv
+
+grep ">" GenE_995_similaridad.fasta > GenE_995_similaridad_id.csv
+
 # Sacar la diferencia entre los dos archivos de ids
 # Secuencias originales Vs secuencias con 97% de similaridad
 
-diff GenE_sort.fasta GenE_97_similaridad.fasta > diff_ideff0_97.csv
+diff GenE_sort_id.fasta GenE_97_similaridad_id.csv > diff_ideff0_97.csv
+
+diff GenE_sort_id.fasta GenE_95_similaridad_id.csv > diff_ideff0_95.csv
+
+diff GenE_sort_id.fasta GenE_99_similaridad_id.csv > diff_ideff0_99.csv
+
+diff GenE_sort_id.fasta GenE_995_similaridad_id.csv > diff_ideff0_995.csv
 
 # Estraer solo los id de las diferencias entre: 
 # Secuencias originales Vs secuencias con 97% de similaridad
 
 grep ">" diff_ideff0_97.csv > diff_ideff_0_97.csv 
 
+grep ">" diff_ideff0_95.csv > diff_ideff_0_95.csv
+
+grep ">" diff_ideff0_99.csv > diff_ideff_0_99.csv
+
+grep ">" diff_ideff0_995.csv > diff_ideff_0_995.csv   
+
 rm diff_ideff0_97.csv
+
+rm diff_ideff0_95.csv
+
+rm diff_ideff0_99.csv
+
+rm diff_ideff0_995.csv
 
 #Ver archivo con los ids de las secuencias con 97% de similaridad en 
 # nano para modificarlo
 
 nano GenE_97_similaridad_id.csv
+
+nano GenE_95_similaridad_id.csv
+
+nano GenE_99_similaridad_id.csv
+
+nano GenE_995_similaridad_id.csv
 
 # Una ves estemos viendo el archivo seguimos las siguientes instrucciones para eliminar ">"
 # ctrl+w (para buscar) y escribimos lo que queremos buscar, en este caso "< >" y enter
