4660b78fc131af19f0108f1e372900d020bb4e91
AndreaSilva <andreasilvacala@gmail.com>
2017-11-21 23:40:57 +0100

    nuevos comentarios, mas entendible. esclarecer la salida que da usearch

--- Piloto_Dengue/bin/usearch_similaridad.sh
+++ Piloto_Dengue/bin/usearch_similaridad.sh
@@ -17,52 +17,54 @@ cd LSB/Piloto_Dengue/data/Secuencias_descargadas/Secuencias_genoma_completo
 
 ./usearch --sort Secuencias_genoma_completo.fasta --output Secuencias_sort.fasta --maxlen 12000 --log log.log
 
-# Identificar y remover las secuencias con el 97% de similaridad
+# Identificar y remover las secuencias con el 95, 97, 99, 99.5, 99.9 % de similaridad y dejar las secuencias Disimilares
+# seedsout son las secuencias disimilares
 
-./usearch --cluster Secuencias_sort.fasta --maxlen 12000 --id 0.95 --iddef 0 --band 1 --seedsout Secuencias_95_similaridad.fasta --uc result.uc --log log_95.log 
+./usearch --cluster Secuencias_sort.fasta --maxlen 12000 --id 0.95 --iddef 0 --band 1 --seedsout Secuencias_95_Disimilaridad.fasta --uc result.uc --log log_95.log 
 
-./usearch --cluster Secuencias_sort.fasta --maxlen 12000 --id 0.97 --iddef 0 --band 1 --seedsout Secuencias_97_similaridad.fasta --uc result.uc --log log_97.log
+./usearch --cluster Secuencias_sort.fasta --maxlen 12000 --id 0.97 --iddef 0 --band 1 --seedsout Secuencias_97_Disimilaridad.fasta --uc result.uc --log log_97.log
 
-./usearch --cluster Secuencias_sort.fasta --maxlen 12000 --id 0.99 --iddef 0 --band 1 --seedsout Secuencias_99_similaridad.fasta --uc result.uc --log log_99.log 
+./usearch --cluster Secuencias_sort.fasta --maxlen 12000 --id 0.99 --iddef 0 --band 1 --seedsout Secuencias_99_Disimilaridad.fasta --uc result.uc --log log_99.log 
 
-./usearch --cluster Secuencias_sort.fasta --maxlen 12000 --id 0.995 --iddef 0 --band 1 --seedsout Secuencias_995_similaridad.fasta --uc result.uc --log log_995.log
+./usearch --cluster Secuencias_sort.fasta --maxlen 12000 --id 0.995 --iddef 0 --band 1 --seedsout Secuencias_995_Disimilaridad.fasta --uc result.uc --log log_995.log
 
-./usearch --cluster Secuencias_sort.fasta --maxlen 12000 --id 0.999 --iddef 0 --band 1 --seedsout Secuencias_999_similaridad.fasta --uc result.uc --log log_999.log 
+./usearch --cluster Secuencias_sort.fasta --maxlen 12000 --id 0.999 --iddef 0 --band 1 --seedsout Secuencias_999_Disimilaridad.fasta --uc result.uc --log log_999.log 
 
 # Conocer las secuencias que fueron eliminadas porque son 97% identicas
 
 #Extraer los ids de cada secuencias:
+
 # 1. de las secuencias originales
 
 grep ">" Secuencias_sort.fasta > Secuencias_sort_id.csv
 
-# 2. de las secuencias con el 97% de similaridad 
+# 2. de las secuencias con el % de Disimilaridad 
 
-grep ">" Secuencias_95_similaridad.fasta > Secuencias_95_similaridad_id.csv
+grep ">" Secuencias_95_Disimilaridad.fasta > Secuencias_95_Disimilaridad_id.csv
 
-grep ">" Secuencias_97_similaridad.fasta > Secuencias_97_similaridad_id.csv
+grep ">" Secuencias_97_Disimilaridad.fasta > Secuencias_97_Disimilaridad_id.csv
 
-grep ">" Secuencias_99_similaridad.fasta > Secuencias_99_similaridad_id.csv
+grep ">" Secuencias_99_Disimilaridad.fasta > Secuencias_99_Disimilaridad_id.csv
 
-grep ">" Secuencias_995_similaridad.fasta > Secuencias_995_similaridad_id.csv
+grep ">" Secuencias_995_Disimilaridad.fasta > Secuencias_995_Disimilaridad_id.csv
 
-grep ">" Secuencias_999_similaridad.fasta > Secuencias_999_similaridad_id.csv
+grep ">" Secuencias_999_Disimilaridad.fasta > Secuencias_999_Disimilaridad_id.csv
 
 # Sacar la diferencia entre los dos archivos de ids
-# Secuencias originales Vs secuencias con 97% de similaridad
+# Secuencias originales Vs secuencias con 97% de Disimilaridad
 
-diff Secuencias_sort_id.csv Secuencias_95_similaridad_id.csv > diff_ideff0_95.csv
+diff Secuencias_sort_id.csv Secuencias_95_Disimilaridad_id.csv > diff_ideff0_95.csv
 
-diff Secuencias_sort_id.csv Secuencias_97_similaridad_id.csv > diff_ideff0_97.csv
+diff Secuencias_sort_id.csv Secuencias_97_Disimilaridad_id.csv > diff_ideff0_97.csv
 
-diff Secuencias_sort_id.csv Secuencias_99_similaridad_id.csv > diff_ideff0_99.csv
+diff Secuencias_sort_id.csv Secuencias_99_Disimilaridad_id.csv > diff_ideff0_99.csv
 
-diff Secuencias_sort_id.csv Secuencias_995_similaridad_id.csv > diff_ideff0_995.csv
+diff Secuencias_sort_id.csv Secuencias_995_Disimilaridad_id.csv > diff_ideff0_995.csv
 
-diff Secuencias_sort_id.csv Secuencias_999_similaridad_id.csv > diff_ideff0_999.csv
+diff Secuencias_sort_id.csv Secuencias_999_Disimilaridad_id.csv > diff_ideff0_999.csv
 
 # Estraer solo los id de las diferencias entre: 
-# Secuencias originales Vs secuencias con 97% de similaridad
+# Secuencias originales Vs secuencias con 97% de Disimilaridad
 
 grep ">" diff_ideff0_95.csv > diff_ideff_0_95.csv 
 
@@ -80,14 +82,24 @@ rm diff_ideff0_99.csv
 rm diff_ideff0_995.csv
 rm diff_ideff0_999.csv
 
-#Ver archivo con los ids de las secuencias con 97% de similaridad en 
+#Ver archivo con los ids de las secuencias con 97% de Disimilaridad y similaridad en 
 # nano para modificarlo
 
-nano Secuencias_95_similaridad_id.csv
-nano Secuencias_97_similaridad_id.csv
-nano Secuencias_99_similaridad_id.csv
-nano Secuencias_995_similaridad_id.csv
-nano Secuencias_999_similaridad_id.csv
+# Disimilares
+
+nano Secuencias_95_Disimilaridad_id.csv
+nano Secuencias_97_Disimilaridad_id.csv
+nano Secuencias_99_Disimilaridad_id.csv
+nano Secuencias_995_Disimilaridad_id.csv
+nano Secuencias_999_Disimilaridad_id.csv
+
+# Similares
+
+nano diff_ideff_0_95.csv
+nano diff_ideff_0_97.csv
+nano diff_ideff_0_99.csv
+nano diff_ideff_0_995.csv
+nano diff_ideff_0_999.csv
 
 # Una ves estemos viendo el archivo seguimos las siguientes instrucciones para eliminar ">"
 # ctrl+w (para buscar) y escribimos lo que queremos buscar, en este caso "< >" y enter
