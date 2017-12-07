aaf99896f83dc98e9d9ce6ce485b6c15093d1d19
AndreaSilva <andreasilvacala@gmail.com>
2017-12-07 15:36:22 +0100

    Corri algunas pruebas y escribi el codigo para la matrix de fractional common

--- Piloto_Dengue/bin/kmer_dist.R
+++ Piloto_Dengue/bin/kmer_dist.R
@@ -9,6 +9,13 @@
 dat_seq <- read.table(file = "/home/andrea/LSB/Piloto_Dengue/data/Piloto_congreso_CCB/genomas_denv2/cosmopolitan/seq_cosmo.txt", header= T, sep=" ")
 
 seq <- dat_seq$seq
+str(dat_seq$seq)
+seq <- c("TCAGGTCATCAGG", "CGGTTACATCGGT", "TCAAAAAAGCTAG", "CAGTAAAAAAGCT" )
+# seq 1 = "AGCGTACGTAGCG"
+# seq 2 = "TCAGGTCATCAGG"
+# seq 3 = "AGCGTACGTAGCG"
+# seq 4 = "CGGTTACATCGGT"
+# seq 5 = "CACGCTAGACTAT"
 
 # Conteo de k-mers para tripletas k=3
 
@@ -18,7 +25,7 @@ kmer3 <-  paste(rep(c("A","C","G","T"),each=16),rep(c("A","C","G","T"),each=4),c
 
 length(kmer3)
 
-result <- t(sapply(seq, stri_count_fixed,pattern=kmer3,overlap=TRUE))
+result <- t(sapply(seq, stri_count_fixed, pattern=kmer3, overlap=TRUE))
 colnames(result) <- kmer3 
 result <- as.data.frame(result)
 
@@ -39,6 +46,7 @@ Euclidian <- function(table){
 }
 
 Euclidiana <- Euclidian(result)
+
 
 Dist_Euclidiana <- write.csv(Euclidiana, file = "/home/andrea/LSB/Piloto_Dengue/data/Matrices_distancias/Dist_Euclidiana.csv")
 
@@ -105,7 +113,7 @@ Dist_Mahalanobis <- write.csv(Mahalanobis, file = "/home/andrea/LSB/Piloto_Dengu
 #########################################################
 # Distancia Conteo comun de k-mer                      ##
 # http://www.ncbi.nlm.nih.gov/pmc/articles/PMC373290/  ##
-# log (0.1 + sum (min(p(s1),p(s2))/(min(l1,l2)-k +1))) ##
+# 1 - (sum (min(p(s1),p(s2))/(min(l1,l2)-k +1)))       ##
 # min(p(s1),p(s2) --> valor minimo del conteo de k-mers #
 # min(l1,l2) --> Valor minimo de longitud de secuencia ##
 # k --> longitud de subsecuencia                       ##
@@ -114,95 +122,113 @@ Dist_Mahalanobis <- write.csv(Mahalanobis, file = "/home/andrea/LSB/Piloto_Dengu
 
 # Calculo de los minimos entre las frecuencias por pares de secuencias
 
-salida <- data.frame()
+#salida <- data.frame()
+#for(i in 1:ncol(result)){
+#  rest <- 1:length(result[,i])
+#  salida_col <- vector()
+#  for(f in 1:length(result[,i])){
+#   res <- rest[-(1:f)]
+#    for(ty in res){
+#      salida_col <- c(salida_col, min(c(result[f,i], result[ty,i])))
+#    }
+#  }
+# salida[1:6,i] <- salida_col
+#}
+#colnames(salida) <- kmer3
+
+soul_sal <- data.frame()
+
 for(i in 1:ncol(result)){
-  rest <- 1:length(result[,i])
-  salida_col <- vector()
+  soul <- vector()
   for(f in 1:length(result[,i])){
-    res <- rest[-(1:f)]
-    for(ty in res){
-      salida_col <- c(salida_col, min(c(result[f,i], result[ty,i])))
+    for(ty in 1:length(result[,i])){
+      soul <- c(soul, min(c(result[f,i], result[ty,i])))
     }
   }
-  salida[1:105,i] <- salida_col
+  soul_sal[1:16,i] <- soul
 }
-View(salida)
 
-colnames(salida) <- kmer3
+sume <- t(apply(soul_sal, 1, sum))
+sume <- as.data.frame(t(sume))
 
 # Calculo de lo minimos entre las longitudes por pares de secuencias
+
+#minlon <- vector()
+#for (i in 1:nrow(lonN)){
+#  algo <- 1:length(lonN[,1])
+#  noalgo <- algo[-(1:i)]
+#  for(n in noalgo){
+#    minlon <- c(minlon,min(lonN[i,1],lonN[n,1]))
+#  }
+#}
+
+#min_lon <- as.data.frame(minlon)
 
 minlon <- vector()
 for (i in 1:nrow(lonN)){
   algo <- 1:length(lonN[,1])
-  noalgo <- algo[-(1:i)]
-  for(n in noalgo){
+  for(n in algo){
     minlon <- c(minlon,min(lonN[i,1],lonN[n,1]))
   }
 }
-
 min_lon <- as.data.frame(minlon)
 
 ###
 # Calculo de la ecuacion completa
 # Fraccional k-mer common
 
-Fractional <- function(table, table1, k=3){
+#Fractional <- function(table, table1, k=3){ # sume, minlon
+#  commonk <- data.frame()
+#  frec <- vector()
+#  lonv <- vector()
+#  for(j in 1:nrow(table)){
+#    frec <- sum(table[j,])
+#    lonv <- sum(table1[j,])
+#    commonk[j,1] <- log(0.1 + frec/(lonv-2))
+#  }
+#  return(commonk)
+#}
+
+#Fraccional_common <- Fractional(salida,min_lon)
+
+Fractional <- function(table, table1, k=3){ # sume, minlon
   commonk <- data.frame()
-  frec <- vector()
-  lonv <- vector()
   for(j in 1:nrow(table)){
-    frec <- sum(table[j,])
-    lonv <- sum(table1[j,])
-    commonk[j,1] <- log(0.1 + frec/(lonv-2))
+    commonk[1,j] <- 1- (table[j,1]/(table1[j,1]-k+1))
   }
   return(commonk)
 }
 
-Fraccional_common <- Fractional(salida,min_lon)
-
+Fractional_common <- Fractional(sume, min_lon)
+Fractional_common <- t(Fractional_common)
 
 #Construcción de la matriz para Fraccional k-mer common
-common <- Fraccional_common$V1
+common <- Fractional_common$V1
 
-mmtx <- matrix(data = 0, nrow = 15, ncol = 15, byrow = FALSE,
+mmtx <- matrix(data = Fractional_common, nrow = 4, ncol = 4, byrow = FALSE,
        dimnames = NULL)
-mmtx[1+1,1]
-
-for (i in 1:nrow(mmtx)){
-  n <- nrow(mmtx)
-  m <- nrow(mmtx)-i
-  mfila <- common[1:m]
-  common <- common[-(1:length(mfila))]
-  mmtx[i:n,i] <- mfila
-}
 
 
-fra_matrix <- data.frame(NA)
-for (i in 1:nrow(result)){
-  n <- nrow(result)
-  m <- nrow(result)-i
-  mfila <- common[1:m]
-  common <- common[-(1:length(mfila))]
-  fra_matrix[1:n,i] <- c(1:i,mfila) 
-}
+#as.matrix(mmtx)
+#for (i in 1:nrow(mmtx)){
+#  n <- nrow(mmtx)
+#  m <- nrow(mmtx)-i
+#  mfila <- common[1:m]
+# common <- common[-(1:length(mfila))]
+#  mmtx[i:n,i] <- mfila
+#}
+
+
+#fra_matrix <- data.frame(NA)
+#for (i in 1:nrow(result)){
+#  n <- nrow(result)
+#  m <- nrow(result)-i
+#  mfila <- common[1:m]
+#  common <- common[-(1:length(mfila))]
+#  fra_matrix[1:n,i] <- c(1:i,mfila) 
+#}
 
 Dist_Fractional <- write.csv(fra_matrix, file = "/home/andrea/LSB/Piloto_Dengue/data/Matrices_distancias/Dist_Fraccional.csv")
-
-salida[1,2:15] <- entrada[1:14,1]
-salida[2,3:15] <- entrada[15:27,1]
-salida[3,4:15] <- entrada[28:39,1]
-salida[4,5:15] <- entrada[40:50,1]
-salida[5,6:15] <- entrada[51:60,1]
-salida[6,7:15] <- entrada[61:69,1]
-salida[7,8:15] <- entrada[70:77,1]
-salida[8,9:15] <- entrada[78:84,1]
-salida[9,10:15] <- entrada[85:90,1]
-salida[10,11:15] <- entrada[91:95,1]
-salida[11,12:15] <- entrada[96:99,1]
-salida[12,13:15] <- entrada[100:102]
-salida[13,14:15] <- entrada[103:104]
-salida[14,15:15] <- entrada[105:105]
 
 #Me falta nombrar las filas y columnas de Salida. PENDIENTE
 #########################################################################################################
